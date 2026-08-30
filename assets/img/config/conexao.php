ed542e961f2bb55f78e2f39e262d2b3411300056
linktudi-droid <linktudi@gmail.com>
2026-08-30 12:18:42 -0300

    Update conexao.php

--- assets/img/config/conexao.php
+++ assets/img/config/conexao.php
@@ -1,1 +1,25 @@
 <?php
+
+$host = "localhost";
+$banco = "linktudi";
+$usuario = "usuario";
+$senha = "senha";
+
+try {
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$banco;charset=utf8",
+        $usuario,
+        $senha
+    );
+
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+} catch (PDOException $e) {
+
+    die("Erro na conexão.");
+
+}
